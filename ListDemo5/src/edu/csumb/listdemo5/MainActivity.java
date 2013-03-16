9a156d8ce4b3e9b40fdf3526ff158417c44843e5
Raymond Alcantara <raymonda@gmail.com>
2013-03-16 14:22:19 -0700

    Raymond - listDemo5 added
    How to's still in progress

---
+++ ListDemo5/src/edu/csumb/listdemo5/MainActivity.java
@@ -0,0 +1,33 @@
+package edu.csumb.listdemo5;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
+/*import android.widget.EditText; */
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void goToCategory(View view) {
+		Intent intent = new Intent(this, Category.class);
+		/*EditText editText = (EditText) findViewById(R.id.edit_message);
+	    String message = editText.getText().toString();
+	    intent.putExtra(EXTRA_MESSAGE, message);*/ 
+	    startActivity(intent);
+	}
+
+}
