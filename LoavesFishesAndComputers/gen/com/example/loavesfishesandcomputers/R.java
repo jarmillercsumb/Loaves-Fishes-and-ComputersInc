2aa62f244a7ce499acd7ec3ed7568950a5efa0a3
Jared Miller <jarmiller@csumb.edu>
2013-03-16 16:08:36 -0700

    New Logo Image

--- LoavesFishesAndComputers/gen/com/example/loavesfishesandcomputers/R.java
+++ LoavesFishesAndComputers/gen/com/example/loavesfishesandcomputers/R.java
@@ -23,17 +23,19 @@ public final class R {
     public static final class drawable {
         public static final int apple=0x7f020000;
         public static final int b2=0x7f020001;
-        public static final int beige=0x7f020002;
-        public static final int but=0x7f020003;
-        public static final int email=0x7f020004;
-        public static final int globe=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int logo=0x7f020007;
-        public static final int logo_basket=0x7f020008;
-        public static final int logo_copy=0x7f020009;
-        public static final int no_back=0x7f02000a;
-        public static final int phone=0x7f02000b;
-        public static final int tutorial_button=0x7f02000c;
+        public static final int basket2=0x7f020002;
+        public static final int beige=0x7f020003;
+        public static final int but=0x7f020004;
+        public static final int email=0x7f020005;
+        public static final int globe=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int logo=0x7f020008;
+        public static final int logo_2=0x7f020009;
+        public static final int logo_basket=0x7f02000a;
+        public static final int logo_copy=0x7f02000b;
+        public static final int no_back=0x7f02000c;
+        public static final int phone=0x7f02000d;
+        public static final int tutorial_button=0x7f02000e;
     }
     public static final class id {
         public static final int action_settings=0x7f080009;
--- LoavesFishesAndComputers/src/com/example/loavesfishesandcomputers/Language.java
+++ LoavesFishesAndComputers/src/com/example/loavesfishesandcomputers/Language.java
@@ -2,7 +2,9 @@ package com.example.loavesfishesandcomputers;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class Language extends Activity {
 
@@ -17,6 +19,10 @@ public class Language extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.language, menu);
 		return true;
+	}
+	public void sendMessage(View view) {
+	    Intent intent = new Intent(this, Help.class);
+	    startActivity(intent);
 	}
 
 }
